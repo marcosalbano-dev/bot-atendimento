093e9d11f97668894f86f7a77315e7ec1e5c7faa
Marcos Albano <marcos.telematia@gmail.com>
2024-04-02 16:29:11 -0300

    Add conn.php e login.php

---
+++ conn.php
@@ -0,0 +1,15 @@
+﻿<?php
+    $host   = "172.28.1.160"; // IP do Banco
+    $user   = "marcos.albano"; // Usuário
+    $pswd   = "Albano@1971"; // Senha
+    $dbname = "bot"; // Banco
+    $port   =  5432;
+
+    $conn = pg_connect("host=$host user=$user port=$port password=$pswd dbname=$dbname") or die (pg_last_error($conn));
+
+    if(!$conn){
+        echo 'deu ruim';
+    } else {
+        echo "deu bom";
+    }
+?>
